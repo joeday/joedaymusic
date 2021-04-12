50c83856f7ade7e2c1f265ea4fb1e5d8c99cb018
Joe Day <joeday@hey.com>
2021-04-12 09:39:14 -0700

    change headline to NEW ALBUM COMING SOON

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <main className="landing-page">
         <header className="site-header">Joe Day</header>
         <section className="site-content">
-          <h1>New album anouncement</h1>
+          <h1>New album coming soon</h1>
           <p className="divider">+ + +</p>
           <p>The new album is done.</p>
           <p>Keep your ear to the ground and be the first to know everything by joining the mailing list.</p>
