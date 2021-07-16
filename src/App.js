a13881c72f8b9de67e9cee938e2cfb61fe83aed1
Joe Day <joeday@hey.com>
2021-07-16 14:22:58 -0700

    changes for album release

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import './App.css';
 import spotifyLogo from './images/Spotify_logo_without_text_white.svg';
 import appleLogo from './images/Apple_Music_Icon_wht_sm_073120.svg';
-import img from './images/all-I-want-digital-small.jpg';
-// import youTubeLogo from './images/yt_icon_mono_dark.png';
+import img from './images/joeday-halflight-final-digital-xs.jpg';
+import bandcampLogo from './images/bc-logotype-light.svg';
 
 function App() {
   return (
     <div className="App">
       <main className="landing-page">
         <header className="site-header">Joe Day</header>
-        
+
         <section className="site-headline">
-          <h1>All I Want Is Home Streaming Everywhere</h1>
+          <h1>Pre order Joe Day's full-length album Halflight</h1>
           <p className="divider">+ + +</p>
         </section>
-        
+
         <section className="site-content">
-          <p>Listen to the first single from Joe’s upcoming full-length album Halflight out August 6th. </p>
-          <a href="https://open.spotify.com/artist/7x1PLGWhJe9SsmZ8i4VGCE?si=yQl5fRWVS0G4I9uIUl1LRQ" rel="noreferrer" target="_blank">
-            <img className="responsive-image-small" src={img} alt="Album cover for the single All I Want Is Home by Joe Day" />
+          <p>Digital album streaming everywhere August 6th. Vinyl shipping in early October.</p>
+          <p><a href="https://joeday.bandcamp.com/album/halflight" rel="noreferrer" target="_blank">Preorder at Bandcamp</a></p>
+          <a href="https://joeday.bandcamp.com/album/halflight" rel="noreferrer" target="_blank">
+            <img className="responsive-image-small" src={img} alt="Album cover for Joe Day's full length album 'Halflight'" />
           </a>
-          <p className="small">Be the first to know everything by joining the mailing list.</p>
+          <p className="small">P.s. My mailing list is the primary way I communicate to the people who care about my music. Join the mailing list and let's talk.</p>
           <div id="revue-embed" className="revue-email-form">
-            <form action="https://www.getrevue.co/profile/joeday/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank">
+            <form action="https://www.getrevue.co/profile/joeday/add_subscriber" method="post" id="revue-form" name="revue-form" target="_blank">
               <input className="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email" />
               <input className="button" type="submit" value="Subscribe" name="member[subscribe]" id="member_submit" />
             </form>
@@ -30,20 +31,23 @@ function App() {
         </section>
 
         <section className="site-sidebar">
-        <a href="https://open.spotify.com/artist/7x1PLGWhJe9SsmZ8i4VGCE?si=yQl5fRWVS0G4I9uIUl1LRQ" rel="noreferrer" target="_blank">
-          <img className="responsive-image" src={img} alt="Album cover for the single All I Want Is Home by Joe Day" />
-        </a>
-          
+          <a href="https://joeday.bandcamp.com/album/halflight" rel="noreferrer" target="_blank">
+            <img className="responsive-image" src={img} alt="Album cover for Joe Day's full length album 'Halflight'" />
+          </a>
+
         </section>
 
         <footer className="site-footer">
           <span className="streaming-links">
-            Listen: 
+            Listen:
             <a className="streaming-icon" href="https://open.spotify.com/artist/7x1PLGWhJe9SsmZ8i4VGCE?si=yQl5fRWVS0G4I9uIUl1LRQ" rel="noreferrer" target="_blank">
               <img src={spotifyLogo} alt="Spotify logo" height="24" />
             </a>
             <a className="streaming-icon" href="https://music.apple.com/us/artist/joe-day/214945054" rel="noreferrer" target="_blank">
               <img src={appleLogo} alt="Apple Music logo" height="24" />
+            </a>
+            <a className="streaming-icon" href="https://joeday.bandcamp.com/album/halflight" rel="noreferrer" target="_blank">
+              <img src={bandcampLogo} alt="Bandcamp logo" height="24" />
             </a>
           </span>
           <a className="photoCredit" href="https://www.janaearlyphotography.com/" rel="noreferrer" target="_blank">Photos by Jana Early</a>
