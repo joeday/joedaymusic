46c97b32af1d9a3056158335e25928fac7081148
Joe Day <joeday@microsoft.com>
2022-04-22 16:05:57 -0700

    update remove prod env from nextjs config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
   experimental: {
     images: {
@@ -8,6 +6,5 @@ module.exports = {
   },
   env: {
     REACT_APP_MAILCHIMP_KEY: '//joedaymusic.us7.list-manage.com/subscribe/post?u=589025a72c5787cb1bc1b7b16&amp;id=c7bc1f5c37'
-  },
-  assetPrefix: isProd ? '/joedaymusic/' : ''
+  }
 }
