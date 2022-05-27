4c0089b8351f42a98882f540fbcec7e466b6b613
Joe Day <joeday@hey.com>
2022-05-27 12:08:10 -0700

    update shows

--- components/Show.js
+++ components/Show.js
@@ -1,27 +1,39 @@
-import PropTypes from 'prop-types';
-import styles from './Show.module.css';
+import PropTypes from "prop-types";
+import styles from "./Show.module.css";
 
-export default function Show (props) {
-  const { date, location, otherBands, onSale, onSaleStartDate, ticketLink} = props;
-  
-  const showDate = new Date(date);
-  const now = new Date();
-  const inFuture = (Date.parse(onSaleStartDate) < Date.parse(now)) ? false : true;
-  console.log(inFuture);
+export default function Show(props) {
+  const { date, location, otherBands, onSale, onSaleStartDate, ticketLink } =
+    props;
 
-  return ( 
+  return (
     <li className={styles.showRow}>
       <section className={styles.basicShowInfo}>
         <div className={styles.date}>{date}</div>
         <div className={styles.cityState}>{location}</div>
-        { otherBands ? <div className={styles.playingWith}><span className={styles.tiny}>with</span>{otherBands}</div> : null }
+        {otherBands ? (
+          <div className={styles.playingWith}>
+            <span className={styles.tiny}>with</span>
+            {otherBands}
+          </div>
+        ) : null}
       </section>
 
       <section className={styles.ticketInfo}>
-        { onSale ? <a className={styles.ticketLink} href={ticketLink} target="_blank" rel="noreferrer">Tickets</a> : <div className={styles.onSaleDate}>On sale: {onSaleStartDate}</div>}
+        {onSale ? (
+          <a
+            className={styles.ticketLink}
+            href={ticketLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tickets
+          </a>
+        ) : (
+          <div className={styles.onSaleDate}>On sale {onSaleStartDate}</div>
+        )}
       </section>
     </li>
-   );
+  );
 }
 
 Show.propTypes = {
@@ -30,5 +42,5 @@ Show.propTypes = {
   otherBands: PropTypes.string,
   onSale: PropTypes.bool,
   onSaleStartDate: PropTypes.string,
-  ticketLink: PropTypes.string
-}
+  ticketLink: PropTypes.string,
+};
--- data/shows.js
+++ data/shows.js
@@ -1,14 +1,40 @@
-const options = { month: '2-digit', day: 'numeric'}
-const onSaleOptions = { month: '2-digit', day: 'numeric', hour: '2-digit', minute: '2-digit'}
+const options = { month: "short", day: "numeric" };
+const onSaleOptions = {
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+};
 
 export default [
-  // {
-  //   id: 0,
-  //   date: new Date('12/18/2022 5:30:00 PM').toLocaleDateString("en-US", options),
-  //   location: 'Chateau Lill, Woodinville, WA',
-  //   otherBands: 'Tim Wilson',
-  //   onSale: false,
-  //   onSaleStartDate: new Date('12/18/2022 5:30:00 PM').toLocaleDateString("en-US", onSaleOptions),
-  //   ticketLink: 'https://www.eventbrite.com/e/221096113427'
-  // }
-]
+  {
+    id: 0,
+    date: new Date("06/24/2022 7:00:00 PM").toLocaleDateString(
+      "en-US",
+      options
+    ),
+    location: "Redmond, WA",
+    otherBands: "John Van Deusen",
+    onSale: true,
+    onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
+      "en-US",
+      onSaleOptions
+    ),
+    ticketLink: "https://sidedooraccess.com/shows/yzMNl0jve1Y6fa0q94wc",
+  },
+  {
+    id: 1,
+    date: new Date("07/22/2022 7:00:00 PM").toLocaleDateString(
+      "en-US",
+      options
+    ),
+    location: "Leavenworth, WA",
+    otherBands: "John Van Deusen",
+    onSale: true,
+    onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
+      "en-US",
+      onSaleOptions
+    ),
+    ticketLink: "https://sidedooraccess.com/shows/BHvSDZopoKUvf3OsAM72",
+  },
+];
