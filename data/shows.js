19e3971c8710859453b72fe0c9ca477b0866f949
Joe Day <joeday@hey.com>
2022-06-11 11:14:06 -0700

    remove leavenworth show

--- data/shows.js
+++ data/shows.js
@@ -22,19 +22,19 @@ export default [
     ),
     ticketLink: "https://sidedooraccess.com/shows/yzMNl0jve1Y6fa0q94wc",
   },
-  {
-    id: 1,
-    date: new Date("07/22/2022 7:00:00 PM").toLocaleDateString(
-      "en-US",
-      options
-    ),
-    location: "Leavenworth, WA",
-    otherBands: "John Van Deusen",
-    onSale: true,
-    onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
-      "en-US",
-      onSaleOptions
-    ),
-    ticketLink: "https://sidedooraccess.com/shows/BHvSDZopoKUvf3OsAM72",
-  },
+  // {
+  //   id: 1,
+  //   date: new Date("07/22/2022 7:00:00 PM").toLocaleDateString(
+  //     "en-US",
+  //     options
+  //   ),
+  //   location: "Leavenworth, WA",
+  //   otherBands: "John Van Deusen",
+  //   onSale: true,
+  //   onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
+  //     "en-US",
+  //     onSaleOptions
+  //   ),
+  //   ticketLink: "https://sidedooraccess.com/shows/BHvSDZopoKUvf3OsAM72",
+  // },
 ];
