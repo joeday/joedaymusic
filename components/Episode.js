a10ce7e689dcdef92b36d33924543c165ff4f8ef
joeday <thejode@gmail.com>
2022-11-29 21:56:10 -0800

    trying to get a start on a card flip transition to no avail

--- components/Episode.js
+++ components/Episode.js
@@ -5,35 +5,41 @@ import Image from "next/image";
 export default function Episode(props) {
   const { title, guest, releaseDate, description, image, id, links } = props;
   return (
-    <div className={styles.episodeCard}>
-      <Image
-        src={image}
-        height="400"
-        width="400"
-        layout="raw"
-        className={styles.episodeImage}
-      />
-      <pre className={styles.episodeMetadata}>
-        Episode {id} - {releaseDate}
-      </pre>
-      <h2 className={styles.episodeTitle}>
-        {title}
-        <br />
-        <small className={styles.tiny}>with</small>
-        {guest}
-      </h2>
+    <div className={styles.episodeCardWrapper}>
+      <div className={styles.episodeCard}>
+        <section className={styles.cardFront}>
+          <Image
+            src={image}
+            height="400"
+            width="400"
+            layout="raw"
+            className={styles.episodeImage}
+          />
+          <pre className={styles.episodeMetadata}>
+            Episode {id} - {releaseDate}
+          </pre>
+          <h2 className={styles.episodeTitle}>
+            {title}
+            <br />
+            <small className={styles.tiny}>with</small>
+            {guest}
+          </h2>
+        </section>
 
-      {/* <p className={styles.episodeDescription}>{description}</p> */}
-      {/* <div className={styles.episodeLinks}>
-        Episode links: &nbsp;
-        {links.length != 0
-          ? links.map((link) => (
-              <a href={link.url} target="_blank" key={link.name}>
-                {link.name}
-              </a>
-            ))
-          : "No episodes"}
-      </div> */}
+        <section className={styles.cardBack}>
+          <p className={styles.episodeDescription}>{description}</p>
+          <div className={styles.episodeLinks}>
+            Episode links: &nbsp;
+            {links.length != 0
+              ? links.map((link) => (
+                  <a href={link.url} target="_blank" key={link.name}>
+                    {link.name}
+                  </a>
+                ))
+              : "No episodes"}
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
