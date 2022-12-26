d563a18d8ad4cecbb6ee9493e86d74b71896d48f
joeday <thejode@gmail.com>
2022-12-26 10:08:26 -0800

    update alt text

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,20 @@ export default function Home() {
     <>
       <Head>
         <meta property="og:title" content="The Halflight Podcast" />
-        <meta property="og:description" content="First episodes drop January 10th. Subscribe now wherever you get your podcasts" />
+        <meta
+          property="og:description"
+          content="First episodes drop January 10th. Subscribe now wherever you get your podcasts"
+        />
         <meta property="og:image" content="/halflight_podcast.png" />
         <meta property="og:url" content="https://joedaymusic.com/podcast" />
         <meta property="og:type" content="website" />
       </Head>
       <Layout home>
         <div className={styles.homeGrid}>
-          <h1 className={styles.headline}>Streaming everywhere <span className="noBreak">January 10</span></h1>
-          <section className={styles.homeContent}>            
+          <h1 className={styles.headline}>
+            Streaming everywhere <span className="noBreak">January 10</span>
+          </h1>
+          <section className={styles.homeContent}>
             <div className={styles.homePageColumn}>
               <Link href="/podcast">
                 <a className={styles.headlineImg}>
@@ -28,16 +33,22 @@ export default function Home() {
                     width="852"
                     height="852"
                     layout="raw"
-                    alt="Cover art for The Halflight Podcast"
+                    alt="Cover art for The Halflight Podcast - out January 10th"
                   />
                 </a>
               </Link>
               <h2 className="subHeading">The Halflight Podcast</h2>
               <p className={styles.description}>
-                Each episode a different guest joins Joe to talk about one song from the album. It’s the audio liner notes to <em>Halflight</em>, and it’s way more than that. It’s a podcast about creativity, loneliness, hope, losing faith, relationships, writer’s block, finding faith, evangelicalism, Mars Hill Church, certainty, doubt, church trauma, healing, wonder, surprise, disappointment, prayer, longing; about making the album <em>Halflight</em>.
+                Each episode a different guest joins Joe to talk about one song
+                from the album. It’s the audio liner notes to <em>Halflight</em>
+                , and it’s way more than that. It’s a podcast about creativity,
+                loneliness, hope, losing faith, relationships, writer’s block,
+                finding faith, evangelicalism, Mars Hill Church, certainty,
+                doubt, church trauma, healing, wonder, surprise, disappointment,
+                prayer, longing; about making the album <em>Halflight</em>.
                 <br />
                 <br />
-                Subscribe now wherever you listen to podcasts. 
+                Subscribe now wherever you listen to podcasts.
               </p>
             </div>
             <div className={styles.homePageColumn}>
@@ -52,14 +63,23 @@ export default function Home() {
               />
               <h2 className="subHeading">The Fear and Love EP</h2>
               <p className={styles.description}>
-                Fear and Love (alternate version) is a brand new version of the song we made to commemorate the release of <em>The Halflight Podcast</em>. In the Fear and Love episode, Brent James Driscoll (creator & producer of the Emmy winning <a href="https://sessionsinplace.com/" target="_blank">Sessions In Place</a>) talk about the original demo from 2017 and how different it is from the album version. The Fear and Love EP chronicles the evolution of one of the most important songs on <em>Halflight</em>. If you were jonesing for some 90s angst, you’re going to be really, really happy. 
+                Fear and Love (alternate version) is a brand new version of the
+                song we made to commemorate the release of{" "}
+                <em>The Halflight Podcast</em>. In the Fear and Love episode,
+                Brent James Driscoll (creator & producer of the Emmy winning{" "}
+                <a href="https://sessionsinplace.com/" target="_blank">
+                  Sessions In Place
+                </a>
+                ) talk about the original demo from 2017 and how different it is
+                from the album version. The Fear and Love EP chronicles the
+                evolution of one of the most important songs on{" "}
+                <em>Halflight</em>. If you were jonesing for some 90s angst,
+                you’re going to be really, really happy.
                 <br />
                 <br />
                 Streaming everywhere January 10th.
               </p>
-              
             </div>
-           
           </section>
         </div>
       </Layout>
