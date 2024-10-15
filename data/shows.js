4846d300898f1f900c61a6758cd6f3495654a6f2
joeday <thejode@gmail.com>
2024-10-15 11:22:21 -0700

    update shows

--- data/shows.js
+++ data/shows.js
@@ -7,38 +7,38 @@ const onSaleOptions = {
 };
 
 export default [
-  {
-    id: 0,
-    date: new Date("08/23/2024 7:00:00 PM").toLocaleDateString(
-      "en-US",
-      options
-    ),
-    location: "The Conway Muse, Conway, WA",
-    otherBands: "Golden Hart, Gabe Archer",
-    onSale: true,
-    onSaleStartDate: new Date("04/03/2022 10:00:00 AM").toLocaleDateString(
-      "en-US",
-      onSaleOptions
-    ),
-    ticketLink:
-      "https://conwaymuse.com/event/joe-day-fri-aug-23-2024-8-pm-conway-muse/",
-  },
-  {
-    id: 1,
-    date: new Date("09/05/2024 7:00:00 PM").toLocaleDateString(
-      "en-US",
-      options
-    ),
-    location: "Miller's, Carnation, WA",
-    otherBands: "Debbie Miller",
-    onSale: true,
-    onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
-      "en-US",
-      onSaleOptions
-    ),
-    ticketLink:
-      "https://millerscarnation.com/event/joe-day-and-the-coyotes-with-debbie-miller/",
-  },
+  // {
+  //   id: 0,
+  //   date: new Date("08/23/2024 7:00:00 PM").toLocaleDateString(
+  //     "en-US",
+  //     options
+  //   ),
+  //   location: "The Conway Muse, Conway, WA",
+  //   otherBands: "Golden Hart, Gabe Archer",
+  //   onSale: true,
+  //   onSaleStartDate: new Date("04/03/2022 10:00:00 AM").toLocaleDateString(
+  //     "en-US",
+  //     onSaleOptions
+  //   ),
+  //   ticketLink:
+  //     "https://conwaymuse.com/event/joe-day-fri-aug-23-2024-8-pm-conway-muse/",
+  // },
+  // {
+  //   id: 1,
+  //   date: new Date("09/05/2024 7:00:00 PM").toLocaleDateString(
+  //     "en-US",
+  //     options
+  //   ),
+  //   location: "Miller's, Carnation, WA",
+  //   otherBands: "Debbie Miller",
+  //   onSale: true,
+  //   onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
+  //     "en-US",
+  //     onSaleOptions
+  //   ),
+  //   ticketLink:
+  //     "https://millerscarnation.com/event/joe-day-and-the-coyotes-with-debbie-miller/",
+  // },
   // {
   //   id: 2,
   //   date: new Date("10/12/2024 6:00:00 PM").toLocaleDateString(
