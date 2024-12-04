bd32e0d07f8d8f327c898752ee7341c12ec343b1
Joe Day <thejode@gmail.com>
2024-12-03 19:08:39 -0800

    add The Halflight Dispatch to main navigation

--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,7 @@ export default function Bio() {
       <article className={styles.contactGrid}>
         <h1 className={styles.articleTitle}>Email is where it's at</h1>
         <section className={styles.section}>
-          <p>Want to go deeper? <a className={styles.emailLink} href="https://buttondown.email/joeday" target="_blank">Subscribe to my email list.</a> I send an email every 3-4 weeks. The emails can be anything from song stories to poems to playlists. But at the heart of the emails is the invitation to 2-way communication. I genuinely enjoy hearing from you and respond to every email that comes in. So, join the email list and let's talk.</p>
+          <p>Want to go deeper? <a className={styles.emailLink} href="https://buttondown.email/joeday" target="_blank">Subscribe to The Halflight Dispatch.</a> I send an email every 3-4 weeks. The emails can be anything from song stories to poems to playlists. But at the heart of the emails is the invitation to 2-way communication. I genuinely enjoy hearing from you and respond to every email that comes in. So, join the email list and let's talk.</p>
           <div className={styles.otherEmails}>
             <h2 className={styles.contactInfo}>Contact info</h2>
             <p>Management: <a className={styles.emailLink} href="mailto:management@joedaymusic.com">management@joedaymusic.com</a></p>
