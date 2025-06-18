d3386036820c9770eaa7808a1b9335a32698b13a
Joe Day <joeday@hey.com>
2025-06-18 16:46:30 -0700

    Add Conway July 11 show

--- data/shows.js
+++ data/shows.js
@@ -9,49 +9,18 @@ const onSaleOptions = {
 export default [
   {
     id: 0,
-    date: new Date("02/13/2025 7:00:00 PM").toLocaleDateString(
-      "en-US",
-      options
-    ),
-    location: "Seattle, WA",
-    otherBands: "Joseph Pennell, Shay Carlucci",
-    onSale: true,
-    onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
-      "en-US",
-      onSaleOptions
-    ),
-    ticketLink:
-      "https://www.ticketweb.com/event/joseph-pennell-joe-day-shay-chop-suey-tickets/14057693?REFID=ARTIST",
-  },
-  {
-    id: 1,
-    date: new Date("03/28/2025 7:00:00 PM").toLocaleDateString(
-      "en-US",
-      options
-    ),
-    location: "Carnation, WA",
-    otherBands: "Mike Antone",
-    onSale: true,
-    onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
-      "en-US",
-      onSaleOptions
-    ),
-    ticketLink:
-      "https://millerscarnation.com/event/joe-day-and-the-coyotes-with-mike-antone/",
-  },
-  {
-    id: 2,
     date: new Date("07/11/2025 6:00:00 PM").toLocaleDateString(
       "en-US",
       options
     ),
     location: "Conway, WA",
-    otherBands: "TBD",
-    onSale: false,
+    otherBands: "hernamewasprincess",
+    onSale: true,
     onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
       "en-US",
       onSaleOptions
     ),
-    ticketLink: "foo",
+    ticketLink:
+      "https://arcadianpublichouse.com/events-1/ola/services/07-11-25-joe-day-and-the-coyotes",
   },
 ];
