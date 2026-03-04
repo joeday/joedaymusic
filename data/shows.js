4bacc6b4e4bee75b301bdcbda2c2aa4eede62fe0
Joe Day <joeday@hey.com>
2026-03-04 09:08:36 -0800

    update shows

--- data/shows.js
+++ data/shows.js
@@ -9,6 +9,22 @@ const onSaleOptions = {
 export default [
   {
     id: 0,
+    date: new Date("03/27/2026 7:00:00 PM").toLocaleDateString(
+      "en-US",
+      options,
+    ),
+    location: "Federal Way, WA",
+    otherBands: "",
+    onSale: true,
+    onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
+      "en-US",
+      onSaleOptions,
+    ),
+    ticketLink:
+      "https://www.eventbrite.com/e/porchlight-presents-joe-day-federal-way-wa-tickets-1984397257616",
+  },
+  {
+    id: 1,
     date: new Date("04/10/2026 7:00:00 PM").toLocaleDateString(
       "en-US",
       options,
@@ -24,7 +40,7 @@ export default [
       "https://millerscarnation.com/event/jackson-emmer-with-joe-day/",
   },
   {
-    id: 0,
+    id: 2,
     date: new Date("08/07/2026 7:00:00 PM").toLocaleDateString(
       "en-US",
       options,
@@ -37,5 +53,20 @@ export default [
       onSaleOptions,
     ),
     ticketLink: "https://app.tickettailor.com/events/joedaymusic/2054888",
+  },
+  {
+    id: 3,
+    date: new Date("09/19/2026 7:00:00 PM").toLocaleDateString(
+      "en-US",
+      options,
+    ),
+    location: "Conway, WA",
+    otherBands: "",
+    onSale: false,
+    onSaleStartDate: new Date("05/27/2022 10:00:00 AM").toLocaleDateString(
+      "en-US",
+      onSaleOptions,
+    ),
+    ticketLink: "",
   },
 ];
